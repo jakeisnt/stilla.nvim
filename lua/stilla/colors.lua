17c83b742f0f3a09866d75323cd48890346e606e
Jacob Chvatal <jake@isnt.online>
2022-01-22 15:24:13 -0500

    progress?

--- lua/stilla/colors.lua
+++ lua/stilla/colors.lua
@@ -2,21 +2,21 @@ local stilla = (vim.o.background == "dark")
 		and {
 			--16 colors
 			stilla0_gui = "#0D0D0D", -- background
-			stilla1_gui = "#0e1111", -- normal black
+			stilla1_gui = "#121414", -- normal black
 			stilla2_gui = "#434C5E", -- nord2 in palette ??
 			stilla3_gui = "#4C566A", -- as below
 		        stilla3_gui_bright = "#8C8C8C", -- out of palette
 			stilla4_gui = "#F2F2F2", -- foreground
-			stilla5_gui = "#F2F2F2", -- normal white
-			stilla6_gui = "#8C8C8C", -- alt white
+			stilla5_gui = "#FAFAFA", -- normal white
+			stilla6_gui = "#FAF5EF", -- alt white
 			stilla7_gui = "#8FBCBB", -- alt cyan
 			stilla8_gui = "#88C0D0", -- cyan
-			stilla9_gui = "#778899", -- blue
+			stilla9_gui = "#ADB2BA", -- function signatures and things like this
 			stilla10_gui = "#5E81AC", -- nord10 in palette ??
-			stilla11_gui = "#A63D40", -- red
+			stilla11_gui = "#BA8082", -- red
 			stilla12_gui = "#d99962", -- yellow
 			stilla13_gui = "#E9B872", -- yellow2
-			stilla14_gui = "white", -- green (not actually green) used for strings, mostly...
+			stilla14_gui = "#8C8C8C", -- green (not actually green) used for strings, mostly...
 			stilla15_gui = "#CD96B3", -- magenta
 			none = "NONE",
 		}
