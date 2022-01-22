6f0c284cfeaf712e30f2d3765c0030cee3db0cd8
Jacob Chvatal <jake@isnt.online>
2022-01-22 15:34:29 -0500

    few more color tweaks

--- lua/stilla/colors.lua
+++ lua/stilla/colors.lua
@@ -10,13 +10,13 @@ local stilla = (vim.o.background == "dark")
 			stilla5_gui = "#FAFAFA", -- normal white
 			stilla6_gui = "#FAF5EF", -- alt white
 			stilla7_gui = "#8FBCBB", -- alt cyan
-			stilla8_gui = "#88C0D0", -- cyan
+			stilla8_gui = "#88B6D0", -- cyan
 			stilla9_gui = "#ADB2BA", -- function signatures and things like this
 			stilla10_gui = "#5E81AC", -- nord10 in palette ??
 			stilla11_gui = "#BA8082", -- red
 			stilla12_gui = "#d99962", -- yellow
 			stilla13_gui = "#E9B872", -- yellow2
-			stilla14_gui = "#8C8C8C", -- green (not actually green) used for strings, mostly...
+			stilla14_gui = "#A19C9A", -- color of strings, mostly
 			stilla15_gui = "#CD96B3", -- magenta
 			none = "NONE",
 		}
